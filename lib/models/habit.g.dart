6aea34ca8c346a17be2c2829d10a01e4728a21e8
Trần Đình Thịnh <156297768+thinhdabezt@users.noreply.github.com>
2025-10-15 23:45:05 +0700

    Fix Hive model

---
+++ lib/models/habit.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'habit.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HabitAdapter extends TypeAdapter<Habit> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Habit read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Habit(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      description: fields[2] as String,
+      streak: fields[3] as int,
+      exp: fields[4] as int,
+      completedToday: fields[5] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Habit obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.streak)
+      ..writeByte(4)
+      ..write(obj.exp)
+      ..writeByte(5)
+      ..write(obj.completedToday);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
